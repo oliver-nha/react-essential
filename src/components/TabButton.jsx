2266e5cfd5e97907e7c5dc657a61bd83591905eb
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2025-03-21 11:06:53 +0800

    output based on state

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,10 @@
 
 
-export default function TabButton({children}) {
+export default function TabButton({children , onSelect}) {
 function handleClick() {
     console.log('Hello World!');
 }
   return (
-   <li><button onClick={handleClick}>{children}</button></li>
+   <li><button onClick={onSelect}>{children}</button></li>
   )
 }
