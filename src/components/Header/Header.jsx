3cb5e56448e04679562dea50c7535fa62ede3265
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2025-03-22 11:58:42 +0800

    add props to children

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import reactImg from "../../assets/react-core-concepts.png";
+import reactImg from '../../assets/react-core-concepts.png';
 import './Header.css';
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
 function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-  }
-  
+  return Math.floor(Math.random() * (max + 1));
+}
+
 export default function Header() {
-    return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {reactDescriptions[genRandomInt(2)]} React concepts you will need for
-          almost any app you are going to build!
-        </p>
-      </header>
-    );
-  }
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
